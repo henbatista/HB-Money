30638465ecc4bf8c1f19a57b05f85a6eab0f2fe5
Henrique Batista <wellington.hen.batista@gmail.com>
2022-03-28 06:19:34 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App(): JSX.Element{
     return (
-    <div className="App">
-      <h1> Hello Henrique</h1>
+    <>
+      <Header />
       <GlobalStyle />
-    </div>
+    </>
   );
 }
