1cc322b25cad7d56d5dd5cfa96bf03d1c90385f8
Henrique Batista <wellington.hen.batista@gmail.com>
2022-03-29 05:41:27 -0300

    Created transition Table

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../Summary";
+import { TransitionTable } from "../TransactionsTable";
 import { Container } from "./style";
 
 export function Dashboard(){
     return(
         <Container>
             <Summary/>
+            <TransitionTable />
         </Container>
     );
 }
