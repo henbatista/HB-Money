ae061a2ebaf0b948f4e98ea64949bad4b842d61a
Henrique Batista <wellington.hen.batista@gmail.com>
2022-04-06 06:00:28 -0300

    routes created for new transactions

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,30 @@ import { App } from './App';
 
 createServer({
   models:{
-    transactions: Model
+    transaction: Model
+  }, 
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelance de Website',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 6000,
+          createdAt: new Date('2022-04-05 21:00:00'),
+        },
+        {
+          id: 2,
+          title: 'alugue',
+          type: 'withdraw',
+          category: 'Casa',
+          amount: 1100,
+          createdAt: new Date('2022-04-15 21:02:00'),
+        }
+      ],
+    })
   }, 
 
   routes(){
